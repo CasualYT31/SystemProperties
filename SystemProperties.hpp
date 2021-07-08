491409439863e462c0ddff5bd4b825e236464355
CasualYT31 <21147925+CasualYT31@users.noreply.github.com>
2021-07-08 17:32:04 +0100

    Basic functionality declared Based on systemInfo

--- SystemProperties.hpp
+++ SystemProperties.hpp
@@ -22,6 +22,87 @@ SOFTWARE.*/
 
 #pragma once
 
-namespace sys {
+#include <string>
 
+/**\file SystemProperties.hpp
+ * \brief This file declares functions allowing the client to query the computer
+ *        for hardware and software details.
+ */
+
+/**
+ * \brief Contains functions allowing the client to query the computer for hardware
+ *        and software details.
+ */
+namespace sys {
+	/**
+	 * \brief Contains functions allowing the client to query the computer for CPU
+	 *        details.
+	 */
+	namespace cpu {
+		/**
+		 * \brief  Retrieves the name of the CPU model.
+		 * \return The user-friendly name of the CPU.
+		 */
+		std::string model();
+
+		/**
+		 * \brief  Retrieves the architecture of the CPU.
+		 * \return The architecture of the CPU.
+		 */
+		std::string architecture();
+	}
+
+	/**
+	 * \brief Contains functions allowing the client to query the computer for
+	 *        memory details.
+	 */
+	namespace mem {
+		/**
+		 * \brief  Retrieves the total available RAM.
+		 * \return The available RAM in MiB.
+		 */
+		std::uint64_t available();
+	}
+
+	/**
+	 * \brief Contains functions allowing the client to query the computer for
+	 *        Operating System (software) details.
+	 */
+	namespace os {
+		/**
+		 * \brief  Retrieves the name of the running Operating System.
+		 * \return The user-friendly name of the Operating System of the computer.
+		 */
+		std::string name();
+
+		/**
+		 * \brief  Retrieves the version of the running Operating System.
+		 * \return The version of the Operating System currently running.
+		 */
+		std::string version();
+	}
+
+	/**
+	 * \brief Contains functions allowing the client to query the computer for GPU
+	 *        details.
+	 */
+	namespace gpu {
+		/**
+		 * \brief  Retrieves the vendor of the installed GPU.
+		 * \return The name of the GPU's vendor.
+		 */
+		std::string vendor();
+
+		/**
+		 * \brief  Retrieves the name of the installed GPU.
+		 * \return The user-friendly name of the GPU.
+		 */
+		std::string name();
+
+		/**
+		 * \brief  Retrieves the version of the graphics driver in use.
+		 * \return The version of the driver.
+		 */
+		std::string driver();
+	}
 }
